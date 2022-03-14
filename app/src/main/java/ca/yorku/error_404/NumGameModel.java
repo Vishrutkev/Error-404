d399512df1c6d0d70be63d5b5bd0d9dd93e24070
dianaspizzirri <ruivoa@my.yorku.ca>
2022-03-13 21:14:14 -0400

    generateRandomNumber & selectDifficulty methods

--- app/src/main/java/ca/yorku/error_404/NumGameModel.java
+++ app/src/main/java/ca/yorku/error_404/NumGameModel.java
@@ -1,18 +1,32 @@
 package ca.yorku.error_404;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class NumGameModel {
 
     /**
      * Method to to generate a random double value
+     *
      * @param difficulty int type for difficulty level user selected
-     * @return int
+     * @return int from 1-9
      */
-    public static int generateRandomNumber(int difficulty) {
-        throw new UnsupportedOperationException();
+    private int difficulty = 0;
+
+    public static int[] generateRandomNumber(int difficulty) {
+        int randNumList[] = {};
+        Random rnd = new Random();
+        for (int i = 0; i < difficulty; i++) {
+            int randNum = rnd.nextInt(9) + 1;
+            randNumList = Arrays.copyOf(randNumList, randNumList.length + 1);
+            randNumList[randNumList.length - 1] = randNum;
+        }
+        return randNumList;
     }
 
     /**
      * Method to redirect user to user_sequence_input_page.xml
+     *
      * @return None
      */
     public void readyButton() {
@@ -22,6 +36,7 @@ public class NumGameModel {
     /**
      * Method to display help icon page popup and call
      * helpPopupExit() when clicked on exit icon
+     *
      * @return None
      */
     public void helpButton() {
@@ -31,6 +46,7 @@ public class NumGameModel {
     /**
      * Method to exit help icon page popup and return
      * to current page user is on
+     *
      * @return None
      */
     public void helpPopupExit() {
@@ -40,6 +56,7 @@ public class NumGameModel {
     /**
      * Method to display correct number sequence and incorrect sequence
      * user inputted and call updateHighScore() and updateLatestScore()
+     *
      * @return None
      */
     public void incorrectInput() {
@@ -49,6 +66,7 @@ public class NumGameModel {
     /**
      * Method to call updateLatestScore() and addToSequence() and redirect
      * user to user_sequence_page.xml with a new sequence to input guess
+     *
      * @return None
      */
     public void correctInput() {
@@ -57,6 +75,7 @@ public class NumGameModel {
 
     /**
      * Method to display return to main page popup
+     *
      * @return None
      */
     public void homeButton() {
@@ -67,6 +86,7 @@ public class NumGameModel {
      * Method to exit main page popup and display yes
      * button to call homePopupYes() and exit, and no
      * button to call homePopupNo() and close popup
+     *
      * @return None
      */
     public void homePopup() {
@@ -77,6 +97,7 @@ public class NumGameModel {
      * Method to exit current page and return to
      * main_page1 if high score equals 0, otherwise
      * return to main_page2
+     *
      * @return None
      */
     public void homePopupYes() {
@@ -85,6 +106,7 @@ public class NumGameModel {
 
     /**
      * Method to close return to main page popup
+     *
      * @return None
      */
     public void homePopupNo() {
@@ -96,15 +118,24 @@ public class NumGameModel {
      * easy (1) = 1 points each correct user guess
      * medium (2) = 2 points each correct user guess
      * hard (3) = 3 points each correct user guess
+     *
      * @return int
      */
-    public int selectDifficulty() {
-        throw new UnsupportedOperationException();
+    public int selectDifficulty(int userselection) {
+        if (userselection == 1) {
+            difficulty = 1;
+        } else if (userselection == 2) {
+            difficulty = 2;
+        } else {
+            difficulty = 3;
+        }
+        return difficulty;
     }
 
     /**
      * Method to call generateRandomNumber() and add
      * a new number to the sequence needed to memorize
+     *
      * @return None
      */
     public void addToSequence() {
@@ -114,6 +145,7 @@ public class NumGameModel {
     /**
      * Method to update user's high score if latestScore
      * is greater than previous high score
+     *
      * @return None
      */
     public void updateHighScore() {
@@ -123,6 +155,7 @@ public class NumGameModel {
     /**
      * Method to update user's latest score whenever user
      * corrects the sequence correctly
+     *
      * @return None
      */
     public void updateLatestScore() {
@@ -133,16 +166,12 @@ public class NumGameModel {
      * Method to check if user's input matches the sequence needed
      * to be memorized and call correctInput() if correct input
      * and call incorrectInput() if incorrect input
+     *
      * @return None
      */
     public void checkInput() {
         throw new UnsupportedOperationException();
     }
-
-
-
-
-
 
 
 }
