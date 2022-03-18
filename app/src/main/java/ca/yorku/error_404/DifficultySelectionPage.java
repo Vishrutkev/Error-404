4ebd10ab1a987826b92f232cb9bc4c297089f04f
Ryan Pham <ryanlepham9@gmail.com>
2022-03-17 23:54:03 -0400

    added popup designs and helpIcon background, created onClick listener for helpIcon ImageButton

--- app/src/main/java/ca/yorku/error_404/DifficultySelectionPage.java
+++ app/src/main/java/ca/yorku/error_404/DifficultySelectionPage.java
@@ -2,13 +2,33 @@ package ca.yorku.error_404;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DifficultySelectionPage extends AppCompatActivity {
+
+    ImageButton helpPopupBtn;
+    Dialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dificulty_selection_page);
+
+        helpPopupBtn = findViewById(R.id.helpIconButton);
+        mDialog = new Dialog(this);
+
+        helpPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDialog.setContentView(R.layout.help_popup);
+                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            }
+        });
     }
+
 }
