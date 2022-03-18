e4ffe7c66fdcb14a7a0f4aa48e3a1351ee6612d4
Ryan Pham <ryanlepham9@gmail.com>
2022-03-18 19:26:28 -0400

    implemented help button popup

--- app/src/main/java/ca/yorku/error_404/DifficultySelectionPage.java
+++ app/src/main/java/ca/yorku/error_404/DifficultySelectionPage.java
@@ -3,6 +3,7 @@ package ca.yorku.error_404;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -11,22 +12,18 @@ import android.widget.ImageButton;
 
 public class DifficultySelectionPage extends AppCompatActivity {
 
-    ImageButton helpPopupBtn;
-    Dialog mDialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dificulty_selection_page);
 
-        helpPopupBtn = findViewById(R.id.helpIconButton);
-        mDialog = new Dialog(this);
+        ImageButton helpPopupBtn = (ImageButton) findViewById(R.id.helpIconButton);
 
         helpPopupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDialog.setContentView(R.layout.help_popup);
-                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                Intent intent = new Intent(DifficultySelectionPage.this, HelpPopup.class);
+                startActivity(intent);
             }
         });
     }
