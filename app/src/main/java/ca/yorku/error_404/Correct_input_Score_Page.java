831ec296d6d038a099857fa18899baa16769424a
Vishrutkev <vishrutk@my.yorku.ca>
2022-03-19 12:38:51 -0400

    Redirected (1) dificulty_selection_page.xml --> number_sequence__page.xml (2) number_sequence__page.xml to user_sequence_inputt_page.xml (3) user_sequence_inputt_page.xml to correct_inputt_score_page.xml

--- app/src/main/java/ca/yorku/error_404/Correct_input_Score_Page.java
+++ app/src/main/java/ca/yorku/error_404/Correct_input_Score_Page.java
@@ -2,13 +2,32 @@ package ca.yorku.error_404;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Correct_input_Score_Page extends AppCompatActivity {
+    private Button nextroundbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.correct_inputt_score_page);
+
+        nextroundbutton = (Button) findViewById(R.id.nextRoundbutton);
+        nextroundbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNumber_Sequence_Page();
+            }
+        });
+
+    }
+
+
+    public void openNumber_Sequence_Page() {
+        Intent intent = new Intent(this, Number_Sequence_Page.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ca/yorku/error_404/DifficultySelectionPage.java
+++ app/src/main/java/ca/yorku/error_404/DifficultySelectionPage.java
@@ -2,13 +2,51 @@ package ca.yorku.error_404;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DifficultySelectionPage extends AppCompatActivity {
+    private Button easybutton;
+    private Button mediumbutton;
+    private Button hardbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dificulty_selection_page);
+
+
+        easybutton = (Button) findViewById(R.id.easyButton);
+        easybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNumber_Sequence_Page();
+            }
+        });
+
+        mediumbutton = (Button) findViewById(R.id.mediumButton);
+        mediumbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNumber_Sequence_Page();
+            }
+        });
+
+        hardbutton = (Button) findViewById(R.id.hardButton);
+        hardbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNumber_Sequence_Page();
+            }
+        });
+
+    }
+
+
+    public void openNumber_Sequence_Page() {
+        Intent intent = new Intent(this, Number_Sequence_Page.class);
+        startActivity(intent);
     }
 }
